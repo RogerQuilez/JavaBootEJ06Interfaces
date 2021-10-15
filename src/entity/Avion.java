bb10a8988702b4a999cbc49b93b53117a30c0a96
Roger <rogeerquilezpalleja@gmail.com>
2021-10-15 08:36:06 +0200

    Refactoring

--- src/entity/Avion.java
+++ src/entity/Avion.java
@@ -1,6 +1,8 @@
 package entity;
 
 public class Avion extends Vehiculo {
+	
+	private static final int ADD_POTENCIA = 3;
 
 	public Avion(int potencia, String nombre) {
 		super(potencia, nombre);
@@ -8,6 +10,6 @@ public class Avion extends Vehiculo {
 
 	@Override
 	public int moverVehiculo(int metros) {
-		return metros + this.getPotencia()*3;
+		return metros + this.getPotencia()*ADD_POTENCIA;
 	}
 }
--- src/entity/Barco.java
+++ src/entity/Barco.java
@@ -2,13 +2,15 @@ package entity;
 
 public class Barco extends Vehiculo {
 	
+	private static final int ADD_POTENCIA = 2;
+	
 	public Barco(int potencia, String nombre) {
 		super(potencia, nombre);
 	}
 
 	@Override
 	public int moverVehiculo(int metros) {
-		return metros + this.getPotencia()*2;
+		return metros + this.getPotencia()*ADD_POTENCIA;
 	}
 
 }
